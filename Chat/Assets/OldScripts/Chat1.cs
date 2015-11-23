e5b81fe5d5ff87b3df0cc86b0d06acaa6483dfee
Justice Frimpong <jfrimpon@andrew.cmu.edu>
2015-11-23 17:41:09 -0500

    renamed oldscripts

---
+++ Chat/Assets/OldScripts/Chat1.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Chat1 : MonoBehaviour {
+
+	public List<string> chatHistory = new List<string> ();
+
+	private string currentMessage = string.Empty;
+
+	private void OnGUI() {
+		GUILayout.BeginHorizontal (GUILayout.Width (280));
+		currentMessage = GUILayout.TextField (currentMessage);
+		if (GUILayout.Button ("send")) {
+			if(!string.IsNullOrEmpty (currentMessage.Trim ()))
+			{
+				GetComponent<NetworkView>().RPC("ChatMessage", RPCMode.AllBuffered, new object[] {currentMessage});
+				currentMessage = string.Empty;
+			}
+		}
+		GUILayout.EndHorizontal ();
+
+		foreach (string c in chatHistory) {
+			GUILayout.Label (c);
+		}
+	}
+
+	[RPC]
+	public void ChatMessage(string message) {
+		chatHistory.Add (message);
+	}
+}
---
+++ Chat/Assets/OldScripts/NetworkMenu1.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkMenu1 : MonoBehaviour {
+
+	public string connectionIP = "128.237.217.77";
+	public int portNumber = 8271;
+
+	private bool connected = false;
+
+	private void OnConnectedToServer() {
+	
+		connected = true;
+	}
+
+	private void OnServerInitialized() {
+	
+		connected = true;
+	}
+
+	private void OnDisconnectedFromServer() {
+		connected = false;
+	}
+
+	private void OnGUI()
+	{
+		if (!connected) {
+			connectionIP = GUILayout.TextField (connectionIP);
+			int.TryParse (GUILayout.TextField (portNumber.ToString()), out portNumber);
+
+			if (GUILayout.Button ("Connect"))
+				Network.Connect (connectionIP, portNumber);
+
+			if (GUILayout.Button ("Host"))
+				Network.InitializeServer (4, portNumber, true);
+		} else {
+			GUILayout.Label ("Connections: " + Network.connections.Length.ToString ());
+		}
+	}
+}
